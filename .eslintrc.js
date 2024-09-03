0ef7d3a473de575e78ab0611415679c7e59bcec2
mcre <mc@mchs-u.net>
2024-09-03 18:22:54 +0900

    プロジェクト作り直し

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,18 @@
+/**
+ * .eslint.js
+ *
+ * ESLint configuration file.
+ */
+
 module.exports = {
   root: true,
   env: {
     node: true,
   },
   extends: [
-    'plugin:vue/vue3-essential',
-    'eslint:recommended',
+    'vuetify',
     '@vue/eslint-config-typescript',
+    './.eslintrc-auto-import.json',
   ],
   rules: {
     'vue/multi-word-component-names': 'off',
--- src/plugins/index.ts
+++ src/plugins/index.ts
@@ -5,16 +5,14 @@
  */
 
 // Plugins
-import { loadFonts } from './webfontloader'
 import vuetify from './vuetify'
-import pinia from '../store'
+import pinia from '../stores'
 import router from '../router'
 
 // Types
 import type { App } from 'vue'
 
 export function registerPlugins (app: App) {
-  loadFonts()
   app
     .use(vuetify)
     .use(router)
