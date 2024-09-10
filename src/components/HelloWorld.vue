3e741f7fefccf1c7dba994aea0843e1926b5e457
mcre <mc@mchs-u.net>
2024-09-10 16:14:41 +0900

    API試行錯誤

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -13,7 +13,14 @@
 
       <v-row>
         <v-col cols="12">
-          <v-card class="py-4" color="surface-variant" image="https://cdn.vuetifyjs.com/docs/images/one/create/feature.png" prepend-icon="mdi-rocket-launch-outline" rounded="lg" variant="outlined">
+          <v-card
+            class="py-4"
+            color="surface-variant"
+            image="https://cdn.vuetifyjs.com/docs/images/one/create/feature.png"
+            prepend-icon="mdi-rocket-launch-outline"
+            rounded="lg"
+            variant="outlined"
+          >
             <template #image>
               <v-img position="top right" />
             </template>
@@ -25,35 +32,114 @@
             <template #subtitle>
               <div class="text-subtitle-1">
                 Replace this page by removing
-                <v-kbd>{{ `<HelloWorld />` }}</v-kbd> in <v-kbd>pages/index.vue</v-kbd>.
+                <v-kbd>{{ `<HelloWorld />` }}</v-kbd> in
+                <v-kbd>pages/index.vue</v-kbd>.
               </div>
             </template>
 
-            <v-overlay opacity=".12" scrim="primary" contained model-value persistent />
+            <v-overlay
+              opacity=".12"
+              scrim="primary"
+              contained
+              model-value
+              persistent
+            />
           </v-card>
         </v-col>
 
         <v-col cols="6">
-          <v-card append-icon="mdi-open-in-new" class="py-4" color="surface-variant" href="https://vuetifyjs.com/" prepend-icon="mdi-text-box-outline" rel="noopener noreferrer" rounded="lg" subtitle="Learn about all things Vuetify in our documentation." target="_blank" title="Documentation" variant="text">
-            <v-overlay opacity=".06" scrim="primary" contained model-value persistent />
+          <v-card
+            append-icon="mdi-open-in-new"
+            class="py-4"
+            color="surface-variant"
+            href="https://vuetifyjs.com/"
+            prepend-icon="mdi-text-box-outline"
+            rel="noopener noreferrer"
+            rounded="lg"
+            subtitle="Learn about all things Vuetify in our documentation."
+            target="_blank"
+            title="Documentation"
+            variant="text"
+          >
+            <v-overlay
+              opacity=".06"
+              scrim="primary"
+              contained
+              model-value
+              persistent
+            />
           </v-card>
         </v-col>
 
         <v-col cols="6">
-          <v-card append-icon="mdi-open-in-new" class="py-4" color="surface-variant" href="https://vuetifyjs.com/introduction/why-vuetify/#feature-guides" prepend-icon="mdi-star-circle-outline" rel="noopener noreferrer" rounded="lg" subtitle="Explore available framework Features." target="_blank" title="Features" variant="text">
-            <v-overlay opacity=".06" scrim="primary" contained model-value persistent />
+          <v-card
+            append-icon="mdi-open-in-new"
+            class="py-4"
+            color="surface-variant"
+            href="https://vuetifyjs.com/introduction/why-vuetify/#feature-guides"
+            prepend-icon="mdi-star-circle-outline"
+            rel="noopener noreferrer"
+            rounded="lg"
+            subtitle="Explore available framework Features."
+            target="_blank"
+            title="Features"
+            variant="text"
+          >
+            <v-overlay
+              opacity=".06"
+              scrim="primary"
+              contained
+              model-value
+              persistent
+            />
           </v-card>
         </v-col>
 
         <v-col cols="6">
-          <v-card append-icon="mdi-open-in-new" class="py-4" color="surface-variant" href="https://vuetifyjs.com/components/all" prepend-icon="mdi-widgets-outline" rel="noopener noreferrer" rounded="lg" subtitle="Discover components in the API Explorer." target="_blank" title="Components" variant="text">
-            <v-overlay opacity=".06" scrim="primary" contained model-value persistent />
+          <v-card
+            append-icon="mdi-open-in-new"
+            class="py-4"
+            color="surface-variant"
+            href="https://vuetifyjs.com/components/all"
+            prepend-icon="mdi-widgets-outline"
+            rel="noopener noreferrer"
+            rounded="lg"
+            subtitle="Discover components in the API Explorer."
+            target="_blank"
+            title="Components"
+            variant="text"
+          >
+            <v-overlay
+              opacity=".06"
+              scrim="primary"
+              contained
+              model-value
+              persistent
+            />
           </v-card>
         </v-col>
 
         <v-col cols="6">
-          <v-card append-icon="mdi-open-in-new" class="py-4" color="surface-variant" href="https://discord.vuetifyjs.com" prepend-icon="mdi-account-group-outline" rel="noopener noreferrer" rounded="lg" subtitle="Connect with Vuetify developers." target="_blank" title="Community" variant="text">
-            <v-overlay opacity=".06" scrim="primary" contained model-value persistent />
+          <v-card
+            append-icon="mdi-open-in-new"
+            class="py-4"
+            color="surface-variant"
+            href="https://discord.vuetifyjs.com"
+            prepend-icon="mdi-account-group-outline"
+            rel="noopener noreferrer"
+            rounded="lg"
+            subtitle="Connect with Vuetify developers."
+            target="_blank"
+            title="Community"
+            variant="text"
+          >
+            <v-overlay
+              opacity=".06"
+              scrim="primary"
+              contained
+              model-value
+              persistent
+            />
           </v-card>
         </v-col>
       </v-row>
@@ -70,7 +156,7 @@ const data = ref<JukugoSearchResponse | null>(null);
 const loading = ref(true);
 const error = ref<string | null>(null);
 
-const baseURL = import.meta.env.VITE_API_BASE_URL;
+const baseURL = `https://${import.meta.env.VITE_API_DOMAIN_NAME}`;
 console.log(baseURL);
 
 const fetchData = async () => {
