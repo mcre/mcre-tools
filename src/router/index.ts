473e385ad83e9b3365bc674605cef191bb6aab1a
mcre <mc@mchs-u.net>
2024-09-20 22:07:53 +0900

    index.htmlをホーム扱いにする

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,11 +2,16 @@ import { createRouter, createWebHistory } from "vue-router/auto";
 import { routes } from "vue-router/auto-routes";
 
 import NotFound from "@/pages/not-found.vue";
+import Index from "@/pages/index.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     ...routes,
+    {
+      path: "/index.html",
+      component: Index,
+    },
     {
       path: "/:pathMatch(.*)*",
       component: NotFound,
