56a755cc6dd32ae477b310aef423ebb8cba5b633
mcre <mc@mchs-u.net>
2024-09-21 17:27:01 +0900

    使ってないパッケージを削除

--- src/plugins/index.ts
+++ src/plugins/index.ts
@@ -1,11 +1,10 @@
 // Plugins
 import vuetify from "./vuetify";
-import pinia from "../stores";
 import router from "../router";
 
 // Types
 import type { App } from "vue";
 
 export function registerPlugins(app: App) {
-  app.use(vuetify).use(router).use(pinia);
+  app.use(vuetify).use(router);
 }
