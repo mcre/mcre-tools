b11512727e70cddd1e722481cb28eb49bd07b44e
mcre <mc@mchs-u.net>
2024-09-25 18:04:25 +0900

    スクロールの挙動が変なので戻す

--- src/router/index.ts
+++ src/router/index.ts
@@ -17,9 +17,6 @@ const router = createRouter({
       component: NotFound,
     },
   ],
-  scrollBehavior(to, from, savedPosition) {
-    return { top: 0 };
-  },
 });
 
 // Workaround for https://github.com/vitejs/vite/issues/11804
