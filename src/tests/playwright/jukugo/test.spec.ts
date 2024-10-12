e92f2e75cdeeef0cf40f5d99706d9ed5c595f652
mcre <mc@mchs-u.net>
2024-10-12 23:47:21 +0900

    Playwrightテストを並列化

--- src/tests/playwright/jukugo/test.spec.ts
+++ src/tests/playwright/jukugo/test.spec.ts
@@ -5,7 +5,6 @@ const positions = ["top", "left", "right", "bottom"] as const;
 async function jukugoTest(
   page: Page,
   expectedAnswer: string,
-  // reverseは矢印ボタンを押し今の状態から逆にすることを意味するので、reverse状態からは通常状態にもどるため注意
   inputs: { c: string; reverse?: boolean }[],
 ) {
   for (let i = 0; i < positions.length; i++) {
@@ -18,45 +17,47 @@ async function jukugoTest(
   await expect(answer).toHaveValue(expectedAnswer);
 }
 
-test("「老」", async ({ page }) => {
-  await page.goto("/jukugo");
-  await page.waitForLoadState("networkidle");
+test.describe.parallel("並列テスト", () => {
+  test("「老」", async ({ page }) => {
+    await page.goto("/jukugo");
+    await page.waitForLoadState("networkidle");
 
-  await jukugoTest(page, "老", [
-    { c: "長" },
-    { c: "海" },
-    { c: "化", reverse: true },
-    { c: "舗", reverse: true },
-  ]);
-});
+    await jukugoTest(page, "老", [
+      { c: "長" },
+      { c: "海" },
+      { c: "化", reverse: true },
+      { c: "舗", reverse: true },
+    ]);
+  });
 
-test("「海」", async ({ page }) => {
-  await page.goto("/jukugo");
-  await page.waitForLoadState("networkidle");
+  test("「海」", async ({ page }) => {
+    await page.goto("/jukugo");
+    await page.waitForLoadState("networkidle");
 
-  await jukugoTest(page, "海", [
-    { c: "近" },
-    { c: "禁", reverse: true },
-    { c: "原", reverse: true },
-    { c: "運", reverse: true },
-  ]);
-});
+    await jukugoTest(page, "海", [
+      { c: "近" },
+      { c: "禁", reverse: true },
+      { c: "原", reverse: true },
+      { c: "運", reverse: true },
+    ]);
+  });
 
-test("「老」逐次＋リセット", async ({ page }) => {
-  await page.goto("/jukugo");
-  await page.waitForLoadState("networkidle");
+  test("「老」逐次＋リセット", async ({ page }) => {
+    await page.goto("/jukugo");
+    await page.waitForLoadState("networkidle");
 
-  await jukugoTest(page, "崎", [{ c: "長" }]);
-  await jukugoTest(page, "野", [{ c: "長" }, { c: "海" }]);
-  await jukugoTest(page, "老", [
-    { c: "長" },
-    { c: "海" },
-    { c: "化", reverse: true },
-  ]);
-  await jukugoTest(page, "老", [
-    { c: "長" },
-    { c: "海" },
-    { c: "化" }, // すでにreverse済みなので何もしない
-    { c: "舗", reverse: true },
-  ]);
+    await jukugoTest(page, "崎", [{ c: "長" }]);
+    await jukugoTest(page, "野", [{ c: "長" }, { c: "海" }]);
+    await jukugoTest(page, "老", [
+      { c: "長" },
+      { c: "海" },
+      { c: "化", reverse: true },
+    ]);
+    await jukugoTest(page, "老", [
+      { c: "長" },
+      { c: "海" },
+      { c: "化" }, // すでにreverse済みなので何もしない
+      { c: "舗", reverse: true },
+    ]);
+  });
 });
