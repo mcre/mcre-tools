d5794988adbb5e16c83a1185c8ca9cf3b813194a
mcre <mc@mchs-u.net>
2024-10-14 01:18:37 +0900

    queryStringのリストを追加

--- src/tests/playwright/jukugo/test.spec.ts
+++ src/tests/playwright/jukugo/test.spec.ts
@@ -7,6 +7,7 @@ async function jukugoTest(
   expectedAnswer: string,
   expectedNumOfAnswers: number,
   inputs: { c: string; reverse?: boolean }[],
+  expectedQueryParams?: Record<string, string>,
 ) {
   for (let i = 0; i < positions.length; i++) {
     const pos = positions[i];
@@ -24,40 +25,79 @@ async function jukugoTest(
   // 読み込み完了
   await expect(answer).toHaveValue(expectedAnswer);
   await expect(numOfAnswers).toHaveText(`${expectedNumOfAnswers} 候補`);
+
+  if (expectedQueryParams) {
+    // クエリパラメータのチェック
+    await page.waitForTimeout(500); // 必要に応じて調整
+    const url = new URL(page.url());
+    const actualParams = Object.fromEntries(url.searchParams.entries());
+
+    expect(actualParams).toEqual(expectedQueryParams);
+  }
 }
 
 test.describe.parallel("並列テスト", () => {
   test("「老」", async ({ page }) => {
     await page.goto("/jukugo");
 
-    await jukugoTest(page, "老", 3, [
-      { c: "長" },
-      { c: "海" },
-      { c: "化", reverse: true },
-      { c: "舗", reverse: true },
-    ]);
+    await jukugoTest(
+      page,
+      "老",
+      3,
+      [
+        { c: "長" },
+        { c: "海" },
+        { c: "化", reverse: true },
+        { c: "舗", reverse: true },
+      ],
+      {
+        t: "長",
+        l: "海",
+        r: "化",
+        b: "舗",
+        ar: "0",
+        ab: "0",
+        a: "老",
+      },
+    );
   });
 
   test("「海」", async ({ page }) => {
     await page.goto("/jukugo");
 
-    await jukugoTest(page, "海", 2, [
-      { c: "近" },
-      { c: "禁", reverse: true },
-      { c: "原", reverse: true },
-      { c: "運", reverse: true },
-    ]);
+    await jukugoTest(
+      page,
+      "海",
+      2,
+      [
+        { c: "近" },
+        { c: "禁", reverse: true },
+        { c: "原", reverse: true },
+        { c: "運", reverse: true },
+      ],
+      {
+        t: "近",
+        l: "禁",
+        r: "原",
+        b: "運",
+        ar: "0",
+        ab: "0",
+        al: "0",
+        a: "海",
+      },
+    );
   });
 
   test("「×」", async ({ page }) => {
     await page.goto("/jukugo");
 
-    await jukugoTest(page, "×", 0, [
-      { c: "近" },
-      { c: "禁" },
-      { c: "原" },
-      { c: "運" },
-    ]);
+    await jukugoTest(
+      page,
+      "×",
+      0,
+      [{ c: "近" }, { c: "禁" }, { c: "原" }, { c: "運" }],
+      { t: "近", l: "禁", r: "原", b: "運", a: "×" },
+    );
   });
 
   test("「老」クエリパラメータ直接指定", async ({ page }) => {
@@ -79,19 +119,31 @@ test.describe.parallel("並列テスト", () => {
   test("「老」逐次＋リセット", async ({ page }) => {
     await page.goto("/jukugo");
 
-    await jukugoTest(page, "崎", 489, [{ c: "長" }]);
-    await jukugoTest(page, "野", 150, [{ c: "長" }, { c: "海" }]);
-    await jukugoTest(page, "老", 22, [
-      { c: "長" },
-      { c: "海" },
-      { c: "化", reverse: true },
-    ]);
-    await jukugoTest(page, "老", 3, [
-      { c: "長" },
-      { c: "海" },
-      { c: "化" }, // すでにreverse済みなので何もしない
-      { c: "舗", reverse: true },
-    ]);
+    await jukugoTest(page, "崎", 489, [{ c: "長" }], { t: "長", a: "崎" });
+    await jukugoTest(page, "野", 150, [{ c: "長" }, { c: "海" }], {
+      t: "長",
+      l: "海",
+      a: "野",
+    });
+    await jukugoTest(
+      page,
+      "老",
+      22,
+      [{ c: "長" }, { c: "海" }, { c: "化", reverse: true }],
+      { t: "長", l: "海", r: "化", ar: "0", a: "老" },
+    );
+    await jukugoTest(
+      page,
+      "老",
+      3,
+      [
+        { c: "長" },
+        { c: "海" },
+        { c: "化" }, // すでにreverse済みなので何もしない
+        { c: "舗", reverse: true },
+      ],
+      { t: "長", l: "海", r: "化", b: "舗", ar: "0", ab: "0", a: "老" },
+    );
 
     await page.locator("#button-reset button").click();
     await Promise.all(
@@ -105,15 +157,43 @@ test.describe.parallel("並列テスト", () => {
 
   test("「老」＋HIDE", async ({ page }) => {
     await page.goto("/jukugo");
-    await jukugoTest(page, "老", 3, [
-      { c: "長" },
-      { c: "海" },
-      { c: "化", reverse: true },
-      { c: "舗", reverse: true },
-    ]);
+    await jukugoTest(
+      page,
+      "老",
+      3,
+      [
+        { c: "長" },
+        { c: "海" },
+        { c: "化", reverse: true },
+        { c: "舗", reverse: true },
+      ],
+      {
+        t: "長",
+        l: "海",
+        r: "化",
+        b: "舗",
+        ar: "0",
+        ab: "0",
+        a: "老",
+      },
+    );
 
     // ==== hideボタンを押す ====
     await page.locator("#button-hide button").click();
+
+    // クエリパラメータのチェック
+    await page.waitForTimeout(500);
+    let url = new URL(page.url());
+    let actualParams = Object.fromEntries(url.searchParams.entries());
+    expect(actualParams).toEqual({
+      t: "長",
+      l: "海",
+      r: "化",
+      b: "舗",
+      ar: "0",
+      ab: "0",
+      h: "1",
+    });
 
     // answerなどが表示されない
     await expect(page.locator("#answer")).not.toBeVisible();
@@ -125,6 +205,20 @@ test.describe.parallel("並列テスト", () => {
 
     // ==== もう一回hideボタンを押す ====
     await page.locator("#button-hide button").click();
+
+    // クエリパラメータのチェック
+    await page.waitForTimeout(500);
+    url = new URL(page.url());
+    actualParams = Object.fromEntries(url.searchParams.entries());
+    expect(actualParams).toEqual({
+      t: "長",
+      l: "海",
+      r: "化",
+      b: "舗",
+      ar: "0",
+      ab: "0",
+      a: "老",
+    });
 
     // answerなどが表示されてる
     const answer = page.locator("#answer");
